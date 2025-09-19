f81b11b4560a6723c422f3bf3dd1ecd53f4aea05
Rainer Stropek <rainer@timecockpit.com>
2025-09-19 10:39:39 +0200

    Saved in TypeScript Playground at 2025-09-19T08:39:38.730Z

--- cat/index.ts
+++ cat/index.ts
@@ -1,30 +1,24 @@
 function setup() {
-createCanvas(800, 800)
-background("skyblue")
+    createCanvas(800, 800)
+    background("skyblue")
 
-fill("lightgrey")
-circle(400, 400, 200)
+    fill("lightgrey")
+    circle(400, 400, 200)
 
-triangle(450, 300, 550, 350, 500, 200)
-triangle(250, 350, 350, 300, 300, 200)
+    triangle(450, 300, 550, 350, 500, 200)
+    triangle(250, 350, 350, 300, 300, 200)
 
-fill("black")
-circle(360, 390, 30)
-circle(440, 390, 30 )
+    fill("black")
+    circle(360, 390, 30)
+    circle(440, 390, 30 )
 
-fill("pink")
-triangle(380, 410, 400, 420, 420, 410 )
+    fill("pink")
+    triangle(380, 410, 400, 420, 420, 410 )
 
-line(400, 420, 400, 450)
-line(400, 450, 430, 470)
-line(400, 450, 370, 470)
+    line(400, 420, 400, 450)
+    line(400, 450, 430, 470)
+    line(400, 450, 370, 470)
 
-line(370, 470, 340, 450)//left mouth
-line(430, 470, 460, 450)//right mouth
-
-
-
-
-
-
-}
+    line(370, 470, 340, 450)//left mouth
+    line(430, 470, 460, 450)//right mou
+    }
