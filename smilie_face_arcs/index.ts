8f77c78dadb7474712fb3b793ebd72be32ad8513
Rainer Stropek <rainer@timecockpit.com>
2025-09-21 20:03:00 +0200

    Saved in TypeScript Playground at 2025-09-21T18:03:00.513Z

---
+++ smilie_face_arcs/index.ts
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+
+angleMode(DEGREES)
+
+stroke("black")
+strokeWeight(10)
+fill("blue")
+
+circle(200, 200, 375)
+
+fill("black")
+circle(125, 125, 20)
+circle(275, 125, 20)
+
+noFill()
+
+arc(200, 250, 200, 150, 25, 180)
+
+
+
+
+
+
+
+
+
+
+
+
+}
