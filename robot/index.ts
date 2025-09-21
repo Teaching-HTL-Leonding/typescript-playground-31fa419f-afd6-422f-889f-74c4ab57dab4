57ac2cbc25bc4304a8dbd8022721c118468ca5d2
Rainer Stropek <rainer@timecockpit.com>
2025-09-21 19:58:23 +0200

    Saved in TypeScript Playground at 2025-09-21T17:58:22.912Z

--- robot/index.ts
+++ robot/index.ts
@@ -1,26 +1,17 @@
 function setup() {
-createCanvas(800, 800)
-background("lightgrey")
-
-circle(400, 150, 25)
-line(400, 100, 400, 300)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    createCanvas(400,400)
+    background("lightgrey")
+    //antene
+    circle(200, 80, 25)
+    line(200, 93, 200, 143)
+    //head
+    fill("grey")
+    square(100, 144, 200)
+    //eyes
+    fill("black")
+    circle(150, 200, 40)
+    circle(250, 200, 40)
+    //mouth
+    fill("white")
+    rect(150, 270, 100, 50)
 }
