3df930e14b99f0e653eeddf82d944a2e0e0cd37c
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 09:10:25 +0200

    Saved in TypeScript Playground at 2025-10-10T07:10:24.578Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -1,19 +1,31 @@
-// <<< ADD CONSTANTS HERE (if you need them)
-
 function setup() {
-createCanvas(1000, 1000);
-background("skyblue");
+createCanvas(1000, 1000)
+background("skyblue")
 
-noStroke();
+noStroke()
 
 
 fill("purple");
-circle(630, 500, 80);
-circle(680, 550, 80);
-circle(740, 500, 80);
-circle(680, 450, 80);
+circle(630, 500, 80)
+circle(680, 550, 80)
+circle(730, 500, 80)
+circle(680, 450, 80)
 
 fill("yellow")
-circle(680, 500, 65);
+circle(680, 500, 65)
 
+angleMode(DEGREES)
+noFill()
+strokeWeight(6)
+stroke("green")
+arc(680, 580, 100, 150, -45, 75)
+
+noStroke()
+fill("yellow")
+circle(250, 500, 65)
+
+fill("purple")
+circle(200, 520, 70)
+circle(240, 560, 70)
+circle(300, 540, 70)
 }
