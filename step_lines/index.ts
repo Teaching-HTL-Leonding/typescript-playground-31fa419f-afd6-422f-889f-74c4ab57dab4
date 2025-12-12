325dbd7f16d424d310c6cfb5d1faefc135156874
Rainer Stropek <rainer@timecockpit.com>
2025-12-12 10:43:00 +0100

    Saved in TypeScript Playground at 2025-12-12T09:42:59.850Z

---
+++ step_lines/index.ts
@@ -0,0 +1,62 @@
+function setup() {
+
+    const SIZE = 400;
+
+    const GRID = 25; 
+ 
+    createCanvas(SIZE, SIZE);
+
+    background("black");
+ 
+    strokeWeight(0.25);
+
+    stroke("lightgray");
+
+    let i = GRID;
+
+    while (i < SIZE) {
+
+        line(i, 0, i, SIZE);
+
+        i += GRID;
+
+    }
+
+    i = GRID;
+
+    while (i < SIZE) {
+
+        line(0, i, SIZE, i);
+
+        i += GRID;
+
+    }
+ 
+    strokeWeight(2);
+ 
+    stroke("yellow");
+
+     i = GRID;
+
+    while (i < SIZE) {
+
+        line(GRID, i, GRID * (i / GRID), i);
+
+        i += GRID;
+
+    }
+ 
+    stroke("red");
+
+    i= GRID
+
+    while(i < SIZE) {
+
+        line(GRID* (i/ GRID), i, SIZE-GRID, i)
+
+        i += GRID
+
+    }
+
+}
+ 
