621523a014ca08412659e3fe0557fa6da2f262af
Rainer Stropek <rainer@timecockpit.com>
2026-02-27 09:44:57 +0100

    Saved in TypeScript Playground at 2026-02-27T08:44:56.650Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,21 +1,45 @@
+let nextFlower = true;
+ 
 function setup() {
-    createCanvas(400, 400);
-    background("hotpink")
+  createCanvas(500, 500);
+  angleMode(DEGREES);
+  flower(nextFlower);
 }
-
-let x: number = 0
-let direction: number = 5
-
-
-function draw() {
-    background("hotpink")
-    fill("darkblue")
-    circle(x, 200, 50)
-    x += direction
-
-    // === gleich
-    // !== nicht gleich
-    // >, >=, <, <= Vergleiche
-    if (x === width){direction = -5  }
-    if (x === 0) { direction = 5}
+ 
+function mouseClicked() {
+  nextFlower = !nextFlower;
+  flower(nextFlower);
 }
+ 
+function flower(style: boolean) {
+  push();
+  translate(random(0, width), random(0, height));
+  scale(0.5)
+ 
+  stroke("black");
+  strokeWeight(4);
+ 
+  let colorNumber = floor(random(3));
+ 
+  if (colorNumber === 0) {
+    fill("red");
+  } else if (colorNumber === 1) {
+    fill("green");
+  } else if (colorNumber === 2) {
+    fill("blue");
+  }
+ 
+  circle(200, 100, 150);
+  circle(300, 200, 150);
+  circle(200, 300, 150);
+  circle(100, 200, 150);
+ 
+  fill("yellow");
+  circle(200, 200, 150);
+
+pop()
+}
+ 
+  
+
+ 
