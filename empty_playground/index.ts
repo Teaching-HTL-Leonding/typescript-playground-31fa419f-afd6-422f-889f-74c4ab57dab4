05f82d81fc9c52371aca4da2e91a56b136e55473
Rainer Stropek <rainer@timecockpit.com>
2026-02-27 10:18:06 +0100

    Saved in TypeScript Playground at 2026-02-27T09:18:06.221Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,31 +1,29 @@
-let nextFlower = true;
+let colorIndex = 0;
  
 function setup() {
   createCanvas(500, 500);
   angleMode(DEGREES);
-  flower(nextFlower);
+  flower(colorIndex);
 }
  
 function mouseClicked() {
-  nextFlower = !nextFlower;
-  flower(nextFlower);
+  colorIndex = (colorIndex + 1) % 3;
+  flower(colorIndex);
 }
  
-function flower(style: boolean) {
+function flower(color) {
   push();
   translate(random(0, width), random(0, height));
-  scale(0.5)
+  scale(0.5);
  
   stroke("black");
   strokeWeight(4);
  
-  let colorNumber = floor(random(3));
- 
-  if (colorNumber === 0) {
+  if (color === 0) {
     fill("red");
-  } else if (colorNumber === 1) {
+  } else if (color === 1) {
     fill("green");
-  } else if (colorNumber === 2) {
+  } else if (color === 2) {
     fill("blue");
   }
  
@@ -36,10 +34,7 @@ function flower(style: boolean) {
  
   fill("yellow");
   circle(200, 200, 150);
-
-pop()
-}
  
-  
-
+  pop();
+}
  
